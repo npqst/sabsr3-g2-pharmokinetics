f1a99e809c993fadb412c455546c880f481e965a
k-shep <92580678+k-shep@users.noreply.github.com>
2021-10-20 15:39:24 +0100

    #17 Adding 3 compartment model

--- pkmodel/models.py
+++ pkmodel/models.py
@@ -20,8 +20,37 @@ class IntravenousModels(AbstractModel):
         transition = self.Q_p1 * (q_c / self.V_c - q_p1 / self.V_p1)
         dqc_dt = self.dose(t, self.X) - q_c / self.V_c * self.CL - transition
         dqp1_dt = transition
-        q_c, q_p1 = y
         return [dqc_dt, dqp1_dt]
+
+    def solve(self):
+        t_eval = np.linspace(0, 1, 1000)
+        y0 = np.array([0.0, 0.0])
+        sol = scipy.integrate.solve_ivp(fun=lambda t, y: self.rhs(t, y),
+                                        t_span=[t_eval[0], t_eval[-1]],
+                                        y0=y0,
+                                        t_eval=t_eval)
+        return Solution(sol)
+
+
+class SubcutaneousModels(AbstractModel):
+    """Class for the subcutaneous model -i.e. 2 peripheral compartment."""
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.CL = parameters['CL']
+        self.q_c = parameters['V_c']
+        self.q_p1 = parameters['V_p1']
+        self.Q_p1 = parameters['Q_p1']
+        self.k_a = parameters['k_a']
+        self.X = parameters['X']
+        self.dose = parameters['dose']
+
+    def rhs(self, t, y):
+        q_c, q_p1, q_0 = y
+        dq0_dt = self.dose(t, X) - self.k_a * q_0
+        transition = self.Q_p1 * (q_c / self.V_c - q_p1 / self.V_p1)
+        dqc_dt = self.k_a * q_0 - q_c / self.V_c * self.CL - transition
+        dqp1_dt = transition
+        return [dq0_dt, dqc_dt, dqp1_dt]
 
     def solve(self):
         t_eval = np.linspace(0, 1, 1000)
