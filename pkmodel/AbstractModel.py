cc08864c63ce0376fcd88cd5a4052271803c290f
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-20 13:23:46 +0100

    #18 flake8 linting implemented

--- pkmodel/AbstractModel.py
+++ pkmodel/AbstractModel.py
@@ -1,11 +1,13 @@
 """
-AbstractModel.py: Abstract class for 1st Order Linear ODE for pharmokinetic model
+AbstractModel.py: Abstract class for 1st Order Linear ODE for pharmokinetic
+model
 Authors: SABS R3 Group 2
 20.10.2021
 """
 from abc import ABC, abstractmethod
 
 from AbstractSolution import AbstractSolution
+
 
 class AbstractModel(ABC):
     @abstractmethod
--- pkmodel/AbstractSolution.py
+++ pkmodel/AbstractSolution.py
@@ -1,9 +1,11 @@
 """
-AbstractSolution.py: Abstract class for solution of 1st Order Linear ODE for pharmokinetic model
+AbstractSolution.py: Abstract class for solution of 1st Order Linear ODE
+for pharmokinetic model
 Authors: SABS R3 Group 2
 20.10.2021
 """
 from abc import ABC, abstractmethod
+
 
 class AbstractSolution(ABC):
     @property
@@ -14,4 +16,3 @@ class AbstractSolution(ABC):
         :raises NotImplementedError: Must be implemented by subclass
         """
         raise NotImplementedError("Abstract method")
-        
