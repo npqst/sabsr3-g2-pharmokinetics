4ec7ef6621d8237a791dd86032b204c8444069c1
guidodee <guydurant@btinternet.com>
2021-10-20 12:06:11 +0100

    #12 First attempt at fixing

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -2,7 +2,7 @@
 # Protocol class
 #
 
-class Protocol:
+class Protocol(Model):
     """A Pharmokinetic (PK) protocol
 
     Parameters
@@ -12,6 +12,21 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self, value=43):
-        self.value = value
+    def __init__(self):
+        self.params = {
+                'name': 'model1',
+                'Q_p1': 1.0,
+                'V_c': 1.0,
+                'V_p1': 1.0,
+                'CL': 1.0,
+                'X': 1.0,
+        }
+    
+    def read_config(self, file_dir):
+        #get current directory and add to file_dir
+        config_file = open("file_dir", "r")
+        
+    
+    def add_parameters
 
+    def generate_model(self, )
