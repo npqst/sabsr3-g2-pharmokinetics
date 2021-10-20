ce055a5fc14c8987be1bc0cbd4afc488afe5ee29
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-20 13:39:09 +0100

    reverted protocol, moved Solution.py to pkmodel

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -2,7 +2,7 @@
 # Protocol class
 #
 
-class Protocol(Model):
+class Protocol:
     """A Pharmokinetic (PK) protocol
 
     Parameters
@@ -12,21 +12,6 @@ class Protocol(Model):
         an example paramter
 
     """
-    def __init__(self):
-        self.params = {
-                'name': 'model1',
-                'Q_p1': 1.0,
-                'V_c': 1.0,
-                'V_p1': 1.0,
-                'CL': 1.0,
-                'X': 1.0,
-        }
-    
-    def read_config(self, file_dir):
-        #get current directory and add to file_dir
-        config_file = open("file_dir", "r")
-        
-    
-    def add_parameters
+    def __init__(self, value=43):
+        self.value = value
 
-    def generate_model(self, )
