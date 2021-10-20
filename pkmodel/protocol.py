8ff0e73ae52ffa36702cea0409d1cb17af9fd810
guidodee <guydurant@btinternet.com>
2021-10-20 13:39:19 +0100

    Updated protocol.py so it takes in a single set of parameters and returns a Model initialised with them

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -2,7 +2,10 @@
 # Protocol class
 #
 
-class Protocol(Model):
+import json
+import unittest
+from model import Model
+class Protocol():
     """A Pharmokinetic (PK) protocol
 
     Parameters
@@ -14,7 +17,7 @@ class Protocol(Model):
     """
     def __init__(self):
         self.params = {
-                'name': 'model1',
+                'name': 'model1' 
                 'Q_p1': 1.0,
                 'V_c': 1.0,
                 'V_p1': 1.0,
@@ -25,8 +28,16 @@ class Protocol(Model):
     def read_config(self, file_dir):
         #get current directory and add to file_dir
         config_file = open("file_dir", "r")
-        
-    
-    def add_parameters
+        dictionaries_list_str = config_file#.split(",") <-- for splitting up multipl dictionaries in the future
+        #dictionaries_list = [json.loads(d) for d in dictionaries_list_str]
+        dictionaries_list = json.loads(dictionaries_list_str)
+        return dictionaries_list
 
-    def generate_model(self, )
+    def fill_parameters(self, param_dicts):
+        for k in self.params.keys:
+            if k not in param_dicts:
+                param_dicts[k] = self.params[k]
+        return param_dicts
+
+    def generate_model(param_dicts):
+        return Model(param_dicts)
