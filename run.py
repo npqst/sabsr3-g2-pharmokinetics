69914b1fd99ef6fd4e3bfd40a770105e1d245808
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2021-10-22 14:25:28 +0100

    update run file

--- run.py
+++ run.py
@@ -6,25 +6,11 @@ usage: python run.py
 """
 
 import pkmodel as pk
-import matplotlib.pyplot as plt
-
 
 protocol = pk.Protocol('pkmodel/config_file.txt')
 model = protocol.generate_model()
 x = model.solve()
 
-print(x.get_solution)
-print(x.get_solution.y.shape)
+x.output()
 
 
-#TODO: move plotting to solution class
-plt.figure()
-
-y1 = x.get_solution.y[0, :]
-y2 = x.get_solution.y[1, :]
-y3 = x.get_solution.y[2, :]
-y4 = x.get_solution.y[3, :]
-t = x.get_solution.t
-plt.plot(t, y1, t, y2, t, y3, t, y4)
-plt.legend(['y1', 'y2', 'y3'])
-plt.show()
--- test.py
+++
@@ -1,9 +0,0 @@
-import pkmodel as pk
-
-protocol = pk.Protocol('pkmodel/config_file.txt')
-model = protocol.generate_model()
-x = model.solve()
-
-x.output()
-
-
