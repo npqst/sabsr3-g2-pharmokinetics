efdb9078d02fcf18a87741b927602efdb683d9c6
k-shep <92580678+k-shep@users.noreply.github.com>
2021-10-22 11:58:22 +0100

    Added tests for models

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,5 +1,5 @@
 import unittest
-# import pkmodel as pk
+import pkmodel as pk
 
 
 class ModelTest(unittest.TestCase):
@@ -10,5 +10,47 @@ class ModelTest(unittest.TestCase):
         """
         Tests Model creation.
         """
-        # model = pk.Model()
-        self.assertEqual(42, 42)
+        parameters = {
+         'name':'test1',
+         'injection_type': 'subcutaneous',
+         'V_c': 1.0,
+         'nr_compartments':1,
+         'periph_1': (5.0, 3.0),
+         'CL': 5.0,
+         'X': 6.0,
+         'dose_mode': 'normal'  
+        }
+        model = pk.models.Model(parameters)
+        expected_model = {
+         'injection_type': 'subcutaneous',
+         'V_c': 1.0,
+         'nr_compartments': 1,
+         'periph_1': (5.0, 3.0),
+         'CL': 5.0,
+         'X': 6.0,
+         'dose_mode': 'normal'  
+        }
+        self.assertEqual(model, expected_model)
+
+        # if __name__ == '__main__':
+            # unittest.main()
+
+    def test_generate_model(self):
+        parameters = {
+         'name' : 'test1',
+         'injection_type': 'subcutaneous',
+         'V_c': 1.0,
+         'nr_compartments':1,
+         'periph_1': (5.0, 3.0),
+         'CL': 5.0,
+         'X': 6.0,
+         'dose_mode': 'normal'  
+        }
+        model = pk.models.Model(parameters)
+        test_array = [[(1,1),1, 1],  [(5.2,3.1), 2, 2.6]]
+        test = model.generate_transition(test_array)
+        expected_transition = [0, 4.65]
+        
+if __name__ == '__main__':
+            unittest.main()
+
