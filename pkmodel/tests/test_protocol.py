925ad8329bdce651eea36c9b70e2dbdd2e4e29ff
guidodee <guydurant@btinternet.com>
2021-10-22 10:32:19 +0100

    Updated code but still not finished testing for protocol.py

--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -1,25 +1,56 @@
 import unittest
-# import pkmodel as pk
 
 
 class ProtocolTest(unittest.TestCase):
     """
     Tests the :class:`Protocol` class.
     """
-    # def test_create(self):
-    #     """
-    #     Tests Protocol creation.
-    #     """
-    #     # model = pk.Protocol()
-    #     self.assertEqual(43, 43)
+    def test_read_config_file(self):
+        """
+        Test read_config_file function.
+        """
+        from pkmodel import Protocol
+        # Will initialise Protocol with default parameters
+        # but will not interfere with the test
+        protocol = Protocol()
+        test_dict = protocol.read_config('pkmodel/tests/testarray1.txt')
+        true_dict = {
+            'name': 'model_test_1',
+            'injection_type': 'intravenous',
+            'V_c': 2.0,
+            'nr_compartments': 1,
+            'periph_1': (5.0, 3.0),
+            'CL': 5.0,
+            'X': 6.0,
+        }
+        self.assertEqual(test_dict, true_dict)
+        test_dict_repeat = protocol.read_config('pkmodel/tests/testarray2.txt')
+        true_dict_repeat = {
+            'name': 'model_test_2',
+            'injection_type': 'intravenous',
+            'V_c': 2.0,
+        }
+        self.assertEqual(test_dict_repeat, true_dict_repeat)
 
-    #     new_dict={}
+    def test_fill_parameters(self):
+        """
+        Test fill_parameters function.
+        """
+        from pkmodel import Protocol
+        protocol = Protocol('pkmodel/tests/testarray2.txt')
+        filled_params = protocol.params
+        true_params = {
+            'name': 'model_test_2',
+            'injection_type': 'intravenous',
+            'V_c': 2.0,
+            'periph_default': (1.0, 1.0),      # (V_p1, Q_p1)
+            'CL': 1.0,
+            'X': 1.0,
+            'dose': 'normal',
+            'nr_compartments': 1,          # nr of peripheral compartments
+        }
+        self.assertEqual(filled_params, true_params)
 
-    def test_generate_model(self):
-        # from pkmodel.protocol import Protocol
-        # protocol = pk.Protocol('pkmodel/tests/testarray1.txt')
-        # with self.assertRaises(Exception) as context:
-        # protocol.generate_model()
-        # self.assertTrue('model type should be either'
-        # ' intravenous or subcutaneous' in context.exception)
-        assert(42 == 42)
+if __name__ == '__main__':
+    unittest.main()
+
