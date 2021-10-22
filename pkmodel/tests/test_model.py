df41c34a116a8648c530bd7af5948c8625287b83
k-shep <92580678+k-shep@users.noreply.github.com>
2021-10-22 12:42:59 +0100

    #67 Added tests for init and generate_model to test protocol andd small change to import in models

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -10,47 +10,43 @@ class ModelTest(unittest.TestCase):
         """
         Tests Model creation.
         """
-        parameters = {
-         'name':'test1',
-         'injection_type': 'subcutaneous',
-         'V_c': 1.0,
-         'nr_compartments':1,
-         'periph_1': (5.0, 3.0),
-         'CL': 5.0,
-         'X': 6.0,
-         'dose_mode': 'normal'  
-        }
+        parameters = {'name': 'test1',
+                      'injection_type': 'subcutaneous',
+                      'V_c': 1.0,
+                      'nr_compartments': 1,
+                      'periph_1': (5.0, 3.0),
+                      'CL': 5.0,
+                      'X': 6.0,
+                      'dose_mode': 'normal'
+                      }
         model = pk.models.Model(parameters)
-        expected_model = {
-         'injection_type': 'subcutaneous',
-         'V_c': 1.0,
-         'nr_compartments': 1,
-         'periph_1': (5.0, 3.0),
-         'CL': 5.0,
-         'X': 6.0,
-         'dose_mode': 'normal'  
-        }
-        self.assertEqual(model, expected_model)
-
-        # if __name__ == '__main__':
-            # unittest.main()
+        expected_model = {'name': 'test1',
+                          'injection_type': 'subcutaneous',
+                          'V_c': 1.0,
+                          'nr_compartments': 1,
+                          'periph_1': (5.0, 3.0),
+                          'CL': 5.0,
+                          'X': 6.0,
+                          'dose_mode': 'normal'
+                          }
+        self.assertEqual(model.parameters, expected_model)
 
     def test_generate_model(self):
-        parameters = {
-         'name' : 'test1',
-         'injection_type': 'subcutaneous',
-         'V_c': 1.0,
-         'nr_compartments':1,
-         'periph_1': (5.0, 3.0),
-         'CL': 5.0,
-         'X': 6.0,
-         'dose_mode': 'normal'  
-        }
+        """
+        Tests generate_model for 3 cases
+        """
+        parameters = {'name': 'test1',
+                      'injection_type': 'subcutaneous',
+                      'V_c': 1.0,
+                      'nr_compartments': 1,
+                      'periph_1': (5.0, 3.0),
+                      'CL': 5.0,
+                      'X': 6.0,
+                      'dose_mode': 'normal'
+                      }
         model = pk.models.Model(parameters)
-        test_array = [[(1,1),1, 1],  [(5.2,3.1), 2, 2.6]]
-        test = model.generate_transition(test_array)
-        expected_transition = [0, 4.65]
-        
-if __name__ == '__main__':
-            unittest.main()
-
+        test_array = [[(1, 1), 1, 1], [(0, 0), 0, 0], [(5.2, 3.1), 2, 2.6]]
+        expected_transition = [0, 0, 4.65]
+        for i in range(0, 1):
+            test = model.generate_transition(*test_array[i])
+            self.assertEqual(test, expected_transition[i])
